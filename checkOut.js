e145f3d391b1584a2ea448a1e01bee2cb247c569
yahya3000 <m.yahya5dec@gmail.com>
2023-10-14 14:02:15 +0200

    created place order banner

---
+++ checkOut.js
@@ -0,0 +1,9 @@
+var fistName = document.getElementById("firstName");
+var lastName = document.getElementById("lastName");
+var email = document.getElementById("email");
+var number = document.getElementById("number");
+var adress = document.getElementById("adress");
+
+function PlaceOrder() {
+    placeOrderBanner.style.display = "block";
+}
